6bcd51a861dbc4f28bddeb18c4d2229399ff8e56
jabestin <jabestin@DESKTOP-20IGLM7>
2025-12-19 09:55:11 +0530

    commit 1

---
+++ Tns_JavaPrograms/src/com/tnsif/oops/inheritance/InheritanceDemo.java
@@ -0,0 +1,24 @@
+package com.tnsif.oops.inheritance;
+
+	class A{
+		void display() {
+			System.out.println("Hellow World");		
+			}
+}
+class B extends A{
+		
+}
+class C extends A{
+	
+}
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+		C obj = new C();
+		obj.display();
+	}
+		// TODO Auto-generated method stub
+
+	}
+
+
